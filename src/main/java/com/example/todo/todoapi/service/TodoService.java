8a9810d66fb8b49fdbf8b2e005b76f981be4e290
kitae1995 <kitae1995@naver.com>
2023-11-27 17:23:25 +0900

    1127-리액트+인텔리제이

--- src/main/java/com/example/todo/todoapi/service/TodoService.java
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.todo.todoapi.service;
 
 import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
 import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
 import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
 import com.example.todo.todoapi.entity.Todo;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,15 +27,13 @@ public class TodoService {
             throws RuntimeException {
         todoRepository.save(requestDTO.toEntity());
         log.info("할 일 저장 완료! 제목: {}", requestDTO.getTitle());
-
         return retrieve();
     }
 
-    //글 목록 작성하기
     public TodoListResponseDTO retrieve() {
         List<Todo> entityList = todoRepository.findAll();
 
-        List<TodoDetailResponseDTO> dtoList // entity를 dto로 바꿔주는 작업
+        List<TodoDetailResponseDTO> dtoList
                 = entityList.stream()
                 /*.map(todo -> new TodoDetailResponseDTO(todo))*/
                 .map(TodoDetailResponseDTO::new)
@@ -42,6 +42,31 @@ public class TodoService {
         return TodoListResponseDTO.builder()
                 .todos(dtoList)
                 .build();
+    }
+
+    public TodoListResponseDTO delete(final String todoId) {
+        try {
+            todoRepository.deleteById(todoId);
+        } catch (Exception e) {
+            log.error("id가 존재하지 않아 삭제에 실패했습니다. - ID: {}, err: {}"
+                    , todoId, e.getMessage());
+            throw new RuntimeException("id가 존재하지 않아 삭제에 실패했습니다.");
+        }
+        return retrieve();
+    }
+
+    public TodoListResponseDTO update(final TodoModifyRequestDTO requestDTO)
+        throws RuntimeException {
+        Optional<Todo> targetEntity
+                = todoRepository.findById(requestDTO.getId());
+
+        targetEntity.ifPresent(todo -> {
+            todo.setDone(requestDTO.isDone());
+
+            todoRepository.save(todo);
+        });
+
+        return retrieve();
     }
 
 
